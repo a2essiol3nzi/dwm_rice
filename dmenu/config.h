2407cc6d8bb0a1d99fb3915de26560b3dc249b05
Alessio Lenzi <alenzi.CS@gmail.com>
2025-06-02 23:17:06 +0200

    DMENU update

--- dmenu/config.h
+++ dmenu/config.h
@@ -1,25 +1,28 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 300;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"HackNerdFont-Regular:size=11"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt = "Run:";      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     		  		fg         bg       */
-	[SchemeNorm] = { "#F6DDCC", "#000000" },
-	[SchemeSel] = { "#fcfcfc", "#565879" },
-	[SchemeOut] = { "#F6DDCC", "#DA6748" },
+    /*     fg         bg       */
+    [SchemeNorm] = { "#c3c4c4", "#101313" },
+    [SchemeSel] = { "#101313", "#88a2aa" },
+    [SchemeOut] = { "#c3c4c4", "#b6babd" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 6;
+static unsigned int lines = 5;
 
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 0;
